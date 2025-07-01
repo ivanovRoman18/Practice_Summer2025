07dc43a2a6731b962a2bd02527296a04d4a501f7
ivanovRoman18 <roma.ivanov149@gmail.com>
2025-07-01 15:40:49 +0600

    feat: add IsPalindrome

---
+++ task01/task01.cs
@@ -0,0 +1,33 @@
+﻿﻿namespace task01;
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+        else
+        {
+            string line = "";
+            input = input.ToLower();
+            foreach (char chr in input )
+            {
+                if (!char.IsPunctuation(chr) && !char.IsWhiteSpace(chr))
+                {
+                    line += chr;
+                }
+            }
+            
+            for (var i = 0; i < input.Length / 2; i++)
+            {
+                if (line[i] != line[line.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }  
+    }
+}
