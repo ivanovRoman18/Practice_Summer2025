2bd93f5f38943c4beb9be7cb0eef7b901a9e5f1b
ivanovRoman18 <roma.ivanov149@gmail.com>
2025-07-04 17:26:01 +0600

    feat: add classAnalyzer

---
+++ task05/task05.cs
@@ -0,0 +1,44 @@
+﻿namespace task05;
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+public class ClassAnalyzer
+{
+    private readonly Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+    {
+        return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                    .Select(m => m.Name);
+    }
+
+    public IEnumerable<string> GetAllFields()
+    {
+        return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Select(f => f.Name);
+    }
+
+    public IEnumerable<string> GetMethodParams(string methodName)
+        => _type.GetMethod(methodName)?
+           .GetParameters()
+           .Select(p => p.Name)
+           .Where(name => name != null)!;
+
+    public IEnumerable<string> GetProperties()
+    {
+        return _type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    .Select(p => p.Name);
+    }
+
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return _type.GetCustomAttributes(typeof(T), true).Length > 0;
+    }
+}
